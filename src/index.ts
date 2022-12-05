64c2ad4a9fe47018a8b61f515c5284441574100f
rhuelgarza03 <rhuelgarza03@gmail.com>
2022-12-05 09:38:54 -0700

    removed greet file, added comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 window.Webflow ||= [];
 window.Webflow.push(() => {
+  // this project is linked to https://webflow-starter-demo.webflow.io/
+
+  // add this attribute to the text element you want the number displayed on
   const counter_selector = '[ts-element="counter"]';
+  // add this attribute to a cms collection item in the webflow designer
   const count_selector = '[ts-element="count"]';
 
   const counter = document.querySelector<HTMLElement>(counter_selector);
